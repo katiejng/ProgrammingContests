8878c6dc78f56f5f27060161643c0852f6487315
Katie <katie.jane.ng@gmail.com>
2019-06-16 19:18:07 +1000

    fb hacker cup

---
+++ Facebook/FacebookHackerCup2019/1.py
@@ -0,0 +1,25 @@
+def main():
+
+    with open('1.out', 'w+') as f:
+        T = int(input())
+
+        for t0 in range(T):
+            line = input()
+
+            if (len(line) == 1):
+                printRes(f, t0, "Y")
+                continue
+            if (line.count(".") == 0):
+                printRes(f, t0, "N")
+                continue
+            if (line.count(".") > line.count("B")):
+                printRes(f, t0, "N")
+                continue
+            printRes(f, t0, "Y")
+
+
+def printRes(f, t0, result):
+    f.write("Case #{}: {}\n".format(t0 + 1, result))
+
+
+main()
