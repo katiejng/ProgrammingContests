7fc25804a11016aa6d3fa653e315218c36ce2be0
Katie <katie.jane.ng@gmail.com>
2019-12-16 16:59:06 +1100

    attempt

---
+++ AdventOfCode/2019/16/16-1.ts
@@ -0,0 +1,29 @@
+import input from "./in16-1";
+function calculate(
+  input: Array<number>,
+  phases: Array<number>,
+  phase: number,
+  index: number
+) {
+  let useful = input.slice(index);
+  let result = useful.map((value, resultIndex) => {
+    let phaseIndex = Math.floor(resultIndex / (phase + 1)) % phases.length;
+    return [value, phases[phaseIndex]];
+  });
+  return result;
+}
+
+function main(input: string, phases: Array<number>) {
+  let current;
+  let next = input.split("").map(x => parseInt(x));
+  for (let i = 0; i < 100; i++) {
+    current = next;
+    next = [];
+    for (let j = 0; j < current.length; j++) {
+      let val = calculate(current, phases, i, j);
+      console.log(val.join("|"));
+    }
+  }
+}
+const phase = [0, 1, 0, -1];
+main(input.zero, phase);
